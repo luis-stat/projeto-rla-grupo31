64f735d3436a40abd1821bb9d9683bb734ae7459
luis-stat <luismiguelpessoal1@gmail.com>
2025-11-10 14:45:59 -0300

    Iniciando as variáveis

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
 <script>
-    
+
+    var notasAv1 = new Array();
+    var notasAv2 = new Array();
+
+    var nota = 0;
+
+    var media = new Array();
+
+    var temp = 0;
+
+    var quantAlunos = parseInt(prompt("Informe a quantidade de alunos: "))
 </script>
