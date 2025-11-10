d6d5864687c9a6511e8b1276ffa321add7fc7909
luis-stat <luismiguelpessoal1@gmail.com>
2025-11-10 14:52:24 -0300

    Inserindo notas no conjunto de notas

--- script.js
+++ script.js
@@ -1,13 +1,17 @@
-<script>
+var notasAv1 = new Array();
+var notasAv2 = new Array();
 
-    var notasAv1 = new Array();
-    var notasAv2 = new Array();
+var nota = 0;
 
-    var nota = 0;
+var media = new Array();
 
-    var media = new Array();
+var temp = 0;
 
-    var temp = 0;
+var quantAlunos = parseInt(prompt("Informe a quantidade de alunos: "))
 
-    var quantAlunos = parseInt(prompt("Informe a quantidade de alunos: "))
-</script>
+for (i = 0; i < quantAlunos; i++) {
+    nota = parseFloat(prompt("Informe a 1a nota do "+ (i + 1) +" aluno: "))
+    notasAv1.push(nota)
+    nota = parseFloat(prompt("Informe a 2a nota do "+ (i + 1) +" aluno: "))
+    notasAv2.push(nota)
+}
